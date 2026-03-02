0c2c01dca8597a87411e7e4f2896c91e5b5120c3
shan961 <roshanroychowdhury@gmail.com>
2026-03-03 02:43:55 +0530

    updated the server and middle ware

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const connectDB = require("./db");
 const cors = require("cors");
-const connectDB = require("./db");   // 👈 THIS LINE
-
-const firRoutes = require("./routes/userRoutes");
 
 const app = express();
-
-// CONNECT DATABASE
-connectDB();   // 👈 MUST BE A FUNCTION
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", firRoutes);
-app.use("/firs/:id",firRoutes);
-app.use("/firs/:id/status",firRoutes);
+connectDB();
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/user", require("./routes/userRoutes"));
+app.use("/api/police", require("./routes/policeRoutes"));
+app.use("/api/admin", require("./routes/adminRoutes"));
+
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
